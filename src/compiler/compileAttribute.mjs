6506299c23838da25195b1a851466e0a394a5a61
Lex <zhang13pro@qq.com>
2021-11-19 17:56:40 +0800

    指令编译

--- src/compiler/compileAttribute.mjs
+++ src/compiler/compileAttribute.mjs
@@ -17,7 +17,48 @@ export default function compileAttribute(node, vm) {
 }
 
 function compileVOnClick(node, method, vm) {
-  console.log("node", node);
-  console.log("method", method);
-  console.log("vm", vm);
+  // 当然避免不了原生DOM操作
+  node.addEventListener("click", function (...args) {
+    vm.$options.methods[method].apply(vm, args);
+  });
+}
+
+function compileVBind(node, attrValue, vm) {
+  const attrName = RegExp.$1;
+  // 移除模版中的 v-bind 属性
+  node.removeAttribute(`v-bind:${attrName}`);
+  // 当属性值发生变化时，重新执行回调函数
+  function cb() {
+    node.setAttribute(attrName, vm[attrValue]);
+  }
+  // 实例化 Watcher，当属性值发生变化时，dep 通知 watcher 执行 update 方法，cb 被执行，重新更新属性
+  new Watcher(cb);
+}
+
+function compileVModel(node, key, vm) {
+  let { tagName, type } = node;
+  tagName = tagName.toLowerCase();
+  // <input type="text" v-model="inputVal" />
+  if (tagName === "input" && type === "text") {
+    // 设置 input 输入框的初始值
+    node.value = vm[key];
+    // 给节点添加 input 事件，当事件发生时更改响应式数据
+    node.addEventListener("input", function () {
+      vm[key] = node.value;
+    });
+  }
+  // <input type="checkbox" v-model="isChecked" />
+  else if (tagName === "input" && type === "checkbox") {
+    node.checked = vm[key];
+    node.addEventListener("change", function () {
+      vm[key] = node.checked;
+    });
+  }
+  // <select v-model="selectedValue"></select>
+  else if (tagName === "select") {
+    node.value = vm[key];
+    node.addEventListener("change", function () {
+      vm[key] = node.value;
+    });
+  }
 }
